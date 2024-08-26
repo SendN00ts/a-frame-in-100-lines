e2661ac686a388f2a88432d4554c4e42163262d6
William Lau <55828115+SendN00ts@users.noreply.github.com>
2024-08-26 14:32:32 +0200

    Deploy Mint.fun mint

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,23 @@ const frameMetadata = getFrameMetadata({
   buttons: [
     {
       action: 'link',
-      label: 'Link to Google',
-      target: 'https://www.google.com',
+      label: 'Mint',
+      target: 'https://mint.fun/base/0x7e9b0d4e50680369ADFa18C0b3b3114Dc30a152E?ref=0xD6Da6129B1EC0C3F9DBe349a4aE564C9cf35be65',
     },
   ],
   image: {
-    src: `${NEXT_PUBLIC_URL}/park-3.png`,
+    src: `${NEXT_PUBLIC_URL}/test.png`,
     aspectRatio: '1:1',
   },
 });
 
 export const metadata: Metadata = {
-  title: 'zizzamia.xyz',
-  description: 'LFG',
+  title: 'FirstTestMint',
+  description: 'test',
   openGraph: {
-    title: 'zizzamia.xyz',
+    title: 'TestMint',
     description: 'LFG',
-    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+    images: [`${NEXT_PUBLIC_URL}/test.png`],
   },
   other: {
     ...frameMetadata,
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <h1>zizzamia.xyz</h1>
+      <h1>TestMint</h1>
     </>
   );
 }
